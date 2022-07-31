0e8beaeaf95a1e8cf168673fcbd6972b129faced
kng <kng@pop-os.localdomain>
2022-07-31 20:20:32 +0530

    login

--- src/App.js
+++ src/App.js
@@ -6,8 +6,30 @@ import React from 'react';
 
 const auth = getAuth();
 function Login(props) {
-  return <div>
-    <button onClick={googleAuth}>Login with Google</button>
+  return <div className='login-body'>
+    <div className="buttons">
+      <button onClick={googleAuth} className="blob-btn">
+        Login with Google
+        <span className="blob-btn__inner">
+          <span className="blob-btn__blobs">
+            <span className="blob-btn__blob"></span>
+            <span className="blob-btn__blob"></span>
+            <span className="blob-btn__blob"></span>
+            <span className="blob-btn__blob"></span>
+          </span>
+        </span>
+      </button>
+      <br />
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+            <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+          </filter>
+        </defs>
+      </svg>
+    </div>
   </div>;
 }
 
@@ -21,10 +43,10 @@ class App extends React.Component {
     onAuthStateChanged(auth, (user) => {
       // console.log("Auth Change")
       if (user) {
-        this.setState({isLoggedIn:true})
+        this.setState({ isLoggedIn: true })
       }
       else {
-        this.setState({isLoggedIn:false})
+        this.setState({ isLoggedIn: false })
       }
     })
 
